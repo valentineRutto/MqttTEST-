3acb66cc878b88259696fd3fe09c75eae8d8b1b0
ValentineRutto <valentinerutto@MacBook-Air-5.local>
2018-08-06 12:07:51 +0300

    connect with mqtt, added pwd and name

--- app/src/main/java/com/example/valentinerutto/test/MainActivity.java
+++ app/src/main/java/com/example/valentinerutto/test/MainActivity.java
@@ -2,16 +2,23 @@ package com.example.valentinerutto.test;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
 import org.eclipse.paho.android.service.MqttAndroidClient;
 import org.eclipse.paho.client.mqttv3.IMqttActionListener;
 import org.eclipse.paho.client.mqttv3.IMqttToken;
 import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
 
 public class MainActivity extends AppCompatActivity {
 
+    static String MQTTHOST ="tcp://broker.hivemq.com:1883";
+    static String topicStr ="";
+    static String USERNAME ="sendy";
+    static String PASSWORD = "93a3a43dbac9ddd362702fb525b42a2d";
+    MqttAndroidClient client;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,9 +26,15 @@ public class MainActivity extends AppCompatActivity {
 
             //create and establish mqttAndroidClient
         String clientId = MqttClient.generateClientId();
-        MqttAndroidClient client = new MqttAndroidClient(this.getApplicationContext(),"tcp://broker.hivemq.com:1883",clientId);
+        client = new MqttAndroidClient(this.getApplicationContext(),MQTTHOST,clientId);
+
+        //connect with mqtt 3.1
+        MqttConnectOptions options = new MqttConnectOptions();
+        options.setUserName(USERNAME);
+        options.setPassword(PASSWORD.toCharArray());
+
         try{
-            IMqttToken token = client.connect();
+            IMqttToken token = client.connect(options);
             token.setActionCallback(new IMqttActionListener() {
                 @Override
                 public void onSuccess(IMqttToken asyncActionToken) {
@@ -39,8 +52,12 @@ public class MainActivity extends AppCompatActivity {
 
         }
 
-
-
-
     }
+ //publish a message
+public  void publish(View v){
+
+}
+
+
+
 }
