0f958d97ec4a8fe7c4739dcd2480c003620d5659
ValentineRutto <valentinerutto@MacBook-Air-5.local>
2018-08-06 12:43:12 +0300

    succesfully published a message

--- app/src/main/java/com/example/valentinerutto/test/MainActivity.java
+++ app/src/main/java/com/example/valentinerutto/test/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.valentinerutto.test;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import org.eclipse.paho.android.service.MqttAndroidClient;
@@ -11,18 +12,31 @@ import org.eclipse.paho.client.mqttv3.IMqttToken;
 import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.MqttPersistenceException;
+
+import java.io.UnsupportedEncodingException;
 
 public class MainActivity extends AppCompatActivity {
 
     static String MQTTHOST ="tcp://broker.hivemq.com:1883";
-    static String topicStr ="";
+    static String topicStr ="test/us";
     static String USERNAME ="sendy";
     static String PASSWORD = "93a3a43dbac9ddd362702fb525b42a2d";
     MqttAndroidClient client;
+    Button publish;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        publish=(Button)findViewById(R.id.button) ;
+        publish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                publish(view);
+            }
+        });
 
             //create and establish mqttAndroidClient
         String clientId = MqttClient.generateClientId();
@@ -54,7 +68,15 @@ public class MainActivity extends AppCompatActivity {
 
     }
  //publish a message
-public  void publish(View v){
+public  void publish(View v)  {
+
+    String topic = topicStr;
+    String message = "hello world from valentine ";
+    try {
+        client.publish(topic, message.getBytes(),0,false);
+    } catch ( MqttException e) {
+        e.printStackTrace();
+    }
 
 }
 
